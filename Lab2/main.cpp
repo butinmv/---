82341012e23878a570da8b97b5b0e35ac7521434
Maxim Butin <butinmv9722@gmail.com>
2020-06-03 22:43:02 +0700

    Initial Commit

---
+++ Lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab2
+//
+//  Created by Maxim Butin on 03.06.2020.
+//  Copyright © 2020 Maxim Butin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
